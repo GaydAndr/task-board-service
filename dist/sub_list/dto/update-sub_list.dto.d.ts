8e5a458198f9d8b3584277689881aeff0de529ee
Andrii Haida <62076338+GaydAndr@users.noreply.github.com>
2024-04-25 23:23:30 +0300

    Delete dist directory

--- dist/sub_list/dto/update-sub_list.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateSubListDto } from './create-sub_list.dto';
-declare const UpdateSubListDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateSubListDto>>;
-export declare class UpdateSubListDto extends UpdateSubListDto_base {
-}
-export {};
